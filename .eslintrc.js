4449f147ca86022baaa8cfa285bc613469ea2856
gangan <gangan.education@gmail.com>
2023-08-08 20:08:37 +0900

    feat: add .eslintrc.js & install packages (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// @ts-check
+/**
+ * @type {import('eslint').ESLint.ConfigData}
+ * @see {@link https://github.com/nuxt/eslint-config/ | Github: nuxt/eslint-config}
+ * @see {@link https://typescript-eslint.io/rules/explicit-function-return-type/ | Rules: explicit-function-return-type}
+ * - NOTE: Return Typesの明示を必須にする
+ */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  extends: ["@nuxt/eslint-config"],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.vue"],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": "error",
+      },
+    },
+  ],
+};
