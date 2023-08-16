3b14d0741583940b3a687e9f91394c3a6a1204b9
gangan <gangan.education@gmail.com>
2023-08-16 23:52:31 +0900

    fix: vitest.config.tsをvite.config.tsに変更 (#11)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 
 import Vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
