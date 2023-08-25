601cfc260a089c81f065f3ccdaac52836fc61bc5
gangan <gangan.education@gmail.com>
2023-08-25 22:44:46 +0900

    fix: 'consistent-type-imports'をデフォルトルールに変更 (#10)

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,18 +15,16 @@ module.exports = {
   extends: ["@nuxt/eslint-config", "prettier"],
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/consistent-type-imports": "off",
     "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      { prefer: "type-imports", disallowTypeAnnotations: true, fixStyle: 'inline-type-imports' }
-    ]
   },
   overrides: [
     {
-      files: ["*.ts", "*.vue"],
+      files: ["*.ts", "*.mts", "*.vue"],
       rules: {
         "@typescript-eslint/explicit-function-return-type": "error",
+        "@typescript-eslint/consistent-type-imports": "error",
       },
     },
   ],
